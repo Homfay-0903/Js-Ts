c784e395017d2322c09760f3bbeff2a006ed22e8
Homfay-0903 <2790165595@qq.com>
2026-05-09 19:50:28 +0800

    s4

--- shoppe/3.js
+++ shoppe/3.js
@@ -1,11 +1,11 @@
 //实现一个函数a，使其奇数次调用时返回1，偶数次调用时返回2（不能使用全局变量）：
-const a = function () {
+const a = (function () {
     let count = 0
     return function () {
         count++
         return count % 2 === 1 ? 1 : 2
     }
-}()
+})()
 
 // example
 console.log(a()); // 1
---
+++ shoppe/4.js
@@ -0,0 +1,48 @@
+// JS实现一个带并发限制的异步调度器Scheduler，保证同时运行的任务最多有两个。完善代码中Scheduler类，
+// 使得以下程序能正确输出
+
+class Scheduler {
+    constructor() {
+        this.queue = []
+        this.count = 0
+        this.maxCount = 2
+    }
+    add(task) {
+        return new Promise((resolve) => {
+            const wrappedTask = () => {
+                return task().then(() => {
+                    this.count--
+                    this.run()
+                    resolve()
+                })
+            }
+            this.queue.push(wrappedTask)
+            this.run()
+        })
+    }
+    run() {
+        while (this.count < this.maxCount && this.queue.length > 0) {
+            this.count++
+            const task = this.queue.shift()
+            task()
+        }
+    }
+}
+
+const timeout = (time) => new Promise(resolve => {
+    setTimeout(resolve, time)
+})
+
+const scheduler = new Scheduler()
+const addTask = (time, order) => {
+    scheduler.add(() => timeout(time)).then(() => console.log(order))
+}
+
+// 执行
+addTask(1000, '1')
+addTask(500, '2')
+addTask(300, '3')
+addTask(400, '4')
+
+
+// output: 2 3 1 4
