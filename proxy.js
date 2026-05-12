44c620e1c4b8951f03c28bad79d33765119cf360
Homfay-0903 <2790165595@qq.com>
2026-05-12 17:33:06 +0800

    proxy1

--- proxy.js
+++ proxy.js
@@ -4,24 +4,30 @@ const target = { name: "小明", age: 18 };
 // 创建代理：给对象装"保安"
 const proxy = new Proxy(target, {
     // 拦截 读取属性
-    get(target, prop) {
-        console.log(`拦截读取：${prop}`);
-        return target[prop];
+    get(target, key) {
+        console.log(`拦截读取：${key}`)
+        return target[key]
     },
 
     // 拦截 修改/新增属性
-    set(target, prop, value) {
-        console.log(`拦截修改：${prop} = ${value}`);
-        target[prop] = value;
-        return true;
+    set(target, key, value) {
+        if (target[key]) {
+            console.log(`拦截修改, ${key} = ${value}`)
+        } else {
+            console.log(`拦截增加, ${key}, and add is ${key} = ${value}`)
+        }
+
+        target[key] = value
+        return true
     },
 
     // 拦截 删除属性
-    deleteProperty(target, prop) {
-        console.log(`拦截删除：${prop}`);
-        delete target[prop];
-        return true;
+    defineProperty(target, key) {
+        console.log(`拦截删除：${key}`)
+        delete target[key]
+        return true
     }
+
 });
 
 // 测试：所有操作都被拦截
