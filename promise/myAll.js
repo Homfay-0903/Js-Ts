3b51b9b14f36d9bd781e3364672a3239e3b239cf
Homfay-0903 <2790165595@qq.com>
2026-06-22 19:30:08 +0800

    myAllsettled

--- promise/myAll.js
+++ promise/myAll.js
@@ -31,5 +31,8 @@ const p1 = Promise.resolve(1);
 const p2 = new Promise(resolve => setTimeout(() => resolve(2), 100));
 //const p2 = Promise.reject('error here')
 const p3 = 3; // 普通值自动包装
-//const p4 = Promise.reject('error here')
-myPromiseAll([p1, p2, p3]).then(res => console.log('成功：', res)).catch(console.error); // 应输出 [1, 2, 3]
+const p4 = Promise.reject('error here')
+
+myPromiseAll([p1, p2, p3, p4])
+    .then(res => console.log('成功：', res)) // 应输出 [1, 2, 3]
+    .catch(err => console.error('faild reason is:', err)); 
---
+++ promise/myAllsettled.js
@@ -0,0 +1,51 @@
+/**
+ * 
+ * @param {*} promises 
+ * @returns 
+ */
+
+function myAllsettled(promises) {
+    return new Promise((resolve, reject) => {
+        if (!Array.isArray(promises)) {
+            return reject(new TypeError('promises must be an Array'))
+        }
+
+        const res = []
+        const promisesLength = promises.length
+
+        if (promisesLength === 0) {
+            return resolve(res)
+        }
+
+        let completedLength = 0
+
+        for (let i = 0; i < promisesLength; i++) {
+            Promise.resolve(promises[i])
+                .then(
+                    (val) => {
+                        res[i] = { status: 'fulfilled', value: val }
+                    },
+                    (reason) => {
+                        res[i] = { status: 'rejected', reason: reason }
+                    }
+                )
+                .finally(() => {
+                    completedLength++
+                    if (completedLength === promisesLength) {
+                        resolve(res)
+                    }
+                })
+        }
+    })
+}
+
+// 测试
+const p1 = Promise.resolve(1);
+const p2 = new Promise(resolve => setTimeout(() => resolve(2), 100));
+//const p2 = Promise.reject('error here')
+const p3 = 3; // 普通值自动包装
+const p4 = Promise.reject('error here')
+
+myAllsettled([p1, p2, p3, p4])
+    .then(res => console.log('成功：', res)) // 应输出 [1, 2, 3]
+    .catch(err => console.error('faild reason is:', err)); 
