87be992130ecda8553750ecb60d0afe674c000d7
Homfay-0903 <2790165595@qq.com>
2026-07-04 20:58:33 +0800

    treetolist

--- js-challenges/treeToList.js
+++ js-challenges/treeToList.js
@@ -1,19 +1,9 @@
-const data = [
-    {
-        id: 1,
-        text: '节点1',
-        parentId: 0,
-        children: [
-            {
-                id: 2,
-                text: '节点1_1',
-                parentId: 1
-            }
-        ]
-    }
-]
-
-function treeToList(tree) {
+/**
+ * 树形结构转列表 - 递归版
+ * @param {Array} tree - 树形数组
+ * @returns {Array} 扁平列表
+ */
+/*function treeToList(tree) {
     const result = []
 
     const traverse = (nodes) => {
@@ -30,5 +20,41 @@ function treeToList(tree) {
     traverse(tree)
 
     console.log(result)
+}*/
+
+function treeToList(tree) {
+    const res = []
+
+    const dfs = (nodes) => {
+        for (const node of nodes) {
+            const { children, ...rest } = node
+
+            if (children) {
+                dfs(children)
+            }
+
+            res.push(rest)
+        }
+    }
+    dfs(tree)
+
+    return res.reverse()
 }
-treeToList(data)
+
+const data = [
+    {
+        id: 1,
+        text: '节点1',
+        parentId: 0,
+        children: [
+            {
+                id: 2,
+                text: '节点1_1',
+                parentId: 1
+            }
+        ]
+    }
+]
+
+const list = treeToList(data)
+console.log(list)
