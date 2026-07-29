5d86984fea719a6905d642d0d64e04362c99f11c
Homfay <2790165595@qq.com>
2026-07-29 17:29:25 +0800

    update: my-map

--- js-challenges/my-map.js
+++ js-challenges/my-map.js
@@ -1,3 +1,4 @@
+/*
 Array.prototype.mymap = function (callback) {
     const res = []
 
@@ -7,6 +8,19 @@ Array.prototype.mymap = function (callback) {
 
     return res
 }
+*/
+
+/**实现数组的map方法 */
+Array.prototype.mymap = function (callback) {
+    const res = []
+
+    for (let i = 0; i < this.length; i++) {
+        res.push(callback(this[i], i, this))
+    }
+
+    return res
+}
+
 
 const arr = [1, 2, 3]
 const res = arr.mymap((element, index) => element = element * 2)
