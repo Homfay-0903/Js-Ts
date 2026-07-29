122e782fa45c8b8070df60ccf3e89d660eddee7f
Homfay <2790165595@qq.com>
2026-07-29 17:58:02 +0800

    update: myInstanceof

--- js-challenges/myInstanceof.js
+++ js-challenges/myInstanceof.js
@@ -1,3 +1,4 @@
+/*
 function myInstanceof(left, right) {
     if (!left) {
         return false
@@ -7,10 +8,21 @@ function myInstanceof(left, right) {
         left.__proto__ === right.prototype || myInstanceof(left.__proto__, right)
     )
 }
+*/
+
+/**实现 instanceof 方法 */
+function myInstanceof(left, right) {
+    if (!left) {
+        return false
+    }
+
+    return left.__proto__ === right.prototype || myInstanceof(left.__proto__, right)
+}
+
 
 function Person() { }
 function Apple() { }
 
 const p = new Person()
 
-console.log(myInstanceof(p, Person))
+console.log(myInstanceof(p, Apple))
