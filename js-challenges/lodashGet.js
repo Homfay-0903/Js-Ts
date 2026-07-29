7a2ccdd43ac2940d5edadfb02938e29cb7c82e31
Homfay <2790165595@qq.com>
2026-07-29 17:10:36 +0800

    update: lodashGet

--- js-challenges/lodashGet.js
+++ js-challenges/lodashGet.js
@@ -1,3 +1,4 @@
+/*
 function _get(obj, path, defaultValue = undefined) {
     const newPath = Array.isArray(path)
         ? path
@@ -8,6 +9,27 @@ function _get(obj, path, defaultValue = undefined) {
     }, obj)
 
     return result === undefined ? defaultValue : result
+}
+*/
+
+/**根据 object对象的path路径获取值。 如果解析 value 是 undefined 会以 defaultValue 取代。 */
+
+/**
+ * 
+ * @param {Object} obj 
+ * @param {Array|string} path 
+ * @param {*} defaultValue 
+ */
+function _get(obj, path, defaultValue = undefined) {
+    const newPath = Array.isArray(path)
+        ? path
+        : path.replace(/\[/g, '.').replace(/\]/g, '').split('.')
+
+    const res = newPath.reduce((curObj, key) => {
+        return curObj == null ? undefined : curObj[key]
+    }, obj)
+
+    return res === undefined ? defaultValue : res
 }
 
 var object = { 'a': [{ 'b': { 'c': 3 } }] };
