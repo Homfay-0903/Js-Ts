e6565ec1646caac016efbe698d09cde3ee7c97a8
Homfay <2790165595@qq.com>
2026-07-29 15:39:05 +0800

    update: concur

--- js-challenges/concurrency.js
+++ js-challenges/concurrency.js
@@ -1,3 +1,4 @@
+/*
 function concurrency(urlArray) {
     const requestArray = urlArray.map((url) => {
         fetch(url).then((response) => {
@@ -10,6 +11,29 @@ function concurrency(urlArray) {
     })
 
     return Promise.any(requestArray)
+}
+*/
+
+//函数介绍：并发请求多个url，返回第一个响应的url
+//参数：urlArray - url数组
+//返回值：Promise对象，resolve为第一个响应的url，reject为错误信息
+
+/**
+ * 
+ * @param {Array} urlList 
+ */
+function concurrency(urlList) {
+    const requestList = urlList.map((url) => {
+        fetch(url).then((response) => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`)
+            } else {
+                return response
+            }
+        })
+    })
+
+    return Promise.any(requestList)
 }
 
 const urls = [
