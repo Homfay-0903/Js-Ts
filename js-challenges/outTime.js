97a80f6c3d232722dde76571ef8dc70eb9513a1a
Homfay <2790165595@qq.com>
2026-07-30 14:00:42 +0800

    update: outTime

--- js-challenges/outTime.js
+++ js-challenges/outTime.js
@@ -1,3 +1,4 @@
+/*
 function outTime() {
     const p1 = new Promise((resolve, reject) => {
         try {
@@ -21,3 +22,43 @@ function outTime() {
 outTime()
     .then((res) => console.log(res))
     .catch((err) => console.log(err))
+*/
+
+/**实现一个返回最快响应结果的函数 */
+function fastestRes(reqList) {
+    return Promise.race(reqList)
+}
+
+const p1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        try {
+            resolve('p1')
+        } catch (error) {
+            reject(error)
+        }
+    }, 1000)
+})
+const p2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        try {
+            resolve('p2')
+        } catch (error) {
+            reject(error)
+        }
+    }, 2000)
+})
+const p3 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        try {
+            reject('p3')
+        } catch (error) {
+            reject(error)
+        }
+    }, 500)
+})
+
+fastestRes([p1, p2, p3])
+    .then(
+        (res) => console.log(res),
+        (err) => console.log(err)
+    )
