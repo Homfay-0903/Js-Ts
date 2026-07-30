5e0697e49945aa2bc2512a10ea578a6aa81f2422
Homfay <2790165595@qq.com>
2026-07-30 14:49:02 +0800

    update: queue

--- js-challenges/queue.js
+++ js-challenges/queue.js
@@ -1,3 +1,4 @@
+/*
 class Queue {
     constructor() {
         this.queue = []
@@ -25,3 +26,42 @@ new Queue()
     .task(2000, () => console.log(2))
     .task(1000, () => console.log(3))
     .start();
+*/
+
+/**实现一个队列，并按入队顺序执行任务 */
+class Queue {
+    constructor() {
+        this.tasksQueue = []
+    }
+
+    addTask(callback, delay) {
+        this.tasksQueue.push({
+            callback,
+            delay
+        })
+
+        return this
+    }
+
+    async executor(params) {
+        for (const task of this.tasksQueue) {
+            await new Promise(resolve => setTimeout(resolve, task.delay))
+            try {
+                task.callback()
+            } catch (error) {
+                console.log('任务执行出错:', error)
+            }
+        }
+    }
+}
+
+(async function () {
+    const queue = new Queue()
+        .addTask(() => console.log(1), 1000)
+        .addTask(() => console.log(2), 2000)
+        .addTask(() => console.log(3), 3000)
+
+    await queue.executor()
+
+    console.log('all reslove')
+})()
