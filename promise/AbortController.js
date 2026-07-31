a8b1be76b8d952ecd328e930fa446477a082e238
Homfay <2790165595@qq.com>
2026-07-31 18:23:03 +0800

    update: abortcontr

--- promise/AbortController.js
+++ promise/AbortController.js
@@ -1,3 +1,4 @@
+/*
 function cancellableDelay(ms) {
     // TODO: 返回 { promise, cancel }
     const controller = new AbortController()
@@ -32,7 +33,53 @@ function cancellableDelay(ms) {
 const { promise, cancel } = cancellableDelay(1000);
 promise
     .then(result => console.log(result))
-    .catch(err => console.log(err)); // 预期 'Cancelled'
+    .catch(err => console.log(err)); 
 
-// 1秒后调用 cancel，此时延迟任务还未完成
-setTimeout(() => cancel(), 1000);
+setTimeout(() => cancel(), 500);
+*/
+
+function cancelFetch(delay) {
+    const controller = new AbortController()
+    const signal = controller.signal
+
+    const promise1 = new Promise((reslove, reject) => {
+        const timerId = setTimeout(() => {
+            try {
+                reslove('success')
+            } catch (error) {
+                reject(error)
+            }
+        }, delay)
+
+        signal.addEventListener('abort', () => {
+            if (timerId) {
+                clearTimeout(timerId)
+            }
+        })
+    })
+
+    const promise2 = new Promise((_, reject) => {
+        signal.addEventListener('abort', () => {
+            reject('cancel')
+        })
+    })
+
+    const race = Promise.race([promise1, promise2])
+
+    return {
+        promise: race,
+        cancel: () => controller.abort()
+    }
+}
+
+const { promise, cancel } = cancelFetch(1000)
+
+promise
+    .then(
+        (res) => console.log(res),
+        (err) => console.log(err)
+    )
+
+// all right
+setTimeout(cancel, 2000)
+setTimeout(() => cancel(), 2000)
