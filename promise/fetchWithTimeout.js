9d875b816aa97f4ed2dd42b15d15ad49dffbf15d
Homfay-0903 <2790165595@qq.com>
2026-08-01 22:03:20 +0800

    update: fetchWithTimeout

--- promise/fetchWithTimeout.js
+++ promise/fetchWithTimeout.js
@@ -1,3 +1,4 @@
+/*
 function fetchWithTimeout(url, timeoutMs) {
     // TODO: 使用 fetch(url) 和 Promise.race 实现超时
     const timeoutPromise = new Promise((_resolved, reject) => {
@@ -17,3 +18,33 @@ fetchWithTimeout('https://jsonplaceholder.typicode.com/todos/1', 5000)
     .then(res => res.json())
     .then(data => console.log(data))
     .catch(err => console.error('错误:', err.message));
+*/
+
+/**实现请求函数，超时则终止 */
+function fetchWithTimeout(yourFetch, timeout) {
+    const timeoutPromise = new Promise((_, reject) => {
+        setTimeout(() => {
+            reject('timeout fetch')
+        }, timeout)
+    })
+
+    return Promise.race([yourFetch, timeoutPromise])
+}
+
+function mockFetch() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            try {
+                resolve('success')
+            } catch (error) {
+                reject(error)
+            }
+        }, 500)
+    })
+}
+
+fetchWithTimeout(mockFetch(), 1000)
+    .then(
+        (res) => console.log(res),
+        (err) => console.log(err)
+    )
