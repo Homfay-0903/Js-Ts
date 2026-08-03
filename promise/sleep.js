f747135242fb3a593e9db5fa316aac1a13441f0e
Homfay <2790165595@qq.com>
2026-08-03 16:42:35 +0800

    update: sleep

--- promise/sleep.js
+++ promise/sleep.js
@@ -1,5 +1,6 @@
+/*
 function sleep(ms) {
-    /* 实现延迟 */
+    /* 实现延迟 
     return new Promise(reslove => setTimeout(reslove, ms))
 }
 
@@ -17,4 +18,38 @@ async function alternateLogs(a, b) {
 }
 
 alternateLogs('A', 'B');
-// 期望输出：A (1秒后) B (1秒后) A (1秒后) B (1秒后) A
+*/
+
+/**实现函数：期望输出：A (1秒后) B (1秒后) A (1秒后) B (1秒后) A */ 
+function abort(delay) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            try {
+                resolve()
+            } catch (error) {
+                reject(error)
+            }
+        }, delay)
+    })
+}
+
+let flag = 0
+async function alternateLogs() {
+    await abort(1500)
+
+    if (flag % 2 === 0) {
+        console.log('a')
+    } else {
+        console.log('b')
+    }
+
+    flag++
+}
+
+async function loop () {
+    while(true) {
+       await alternateLogs()
+    }
+}
+
+loop()
