44d048d7aeebf113116a489c71d9b18c90cc08da
Homfay <2790165595@qq.com>
2026-08-03 15:03:57 +0800

    update: myAny

--- promise/myAny.js
+++ promise/myAny.js
@@ -1,8 +1,3 @@
-/**
- * 手写实现 Promise.any
- * @param {Array} promises - 可迭代对象（如数组）
- * @returns {Promise}
- */
 /*
 Promise.myAny = function (promiseList) {
     return new Promise((reslove, reject) => {
@@ -38,36 +33,43 @@ Promise.myAny = function (promiseList) {
     })
 }
 */
+/**实现Promise.any */
+
+/**
+ * 手写实现 Promise.any
+ * @param {Array} promises - 可迭代对象（如数组）
+ * @returns {Promise}
+ */
 Promise.myAny = function (promises) {
     return new Promise((resolve, reject) => {
+        if (!Array.isArray(promises)) {
+            return reject(new TypeError('promises must be an Array'))
+        }
+
+        const errors = []
+        const n = promises.length
+
+        if (n === 0) {
+            return reject(new AggregateError([], 'promises can not be null'))
+        }
+
+        let rejectedCount = 0
+
         try {
-            //const promises = Array.from(promiseList)
-            const len = promises.length
-            const errors = []
-
-            if (!len) {
-                return reject(new AggregateError([], 'promises can not be null'))
-            }
-
-            let rejectCount = 0
-
-            promises.forEach((promise, index) => {
-                Promise.resolve(promise)
+            for (let i = 0; i < n; i++) {
+                Promise.resolve(promises[i])
                     .then(
-                        (res) => {
-                            resolve(res)
-                        },
-                        (error) => {
-                            rejectCount++
+                        (val) => resolve(val),
+                        (err) => {
+                            rejectedCount++
+                            errors[i] = err
 
-                            errors[index] = error
-
-                            if (rejectCount === len) {
-                                reject(new AggregateError(errors, 'all promises were failed'))
+                            if (rejectedCount === n) {
+                                reject(new AggregateError(errors, 'All promises were rejected'))
                             }
                         }
                     )
-            })
+            }
         } catch (error) {
             reject(error)
         }
@@ -75,21 +77,21 @@ Promise.myAny = function (promises) {
 }
 
 
-const p1 = new Promise((resolve) => setTimeout(() => resolve('p1 finish'), 1000))
-const p2 = new Promise((_, reject) => setTimeout(() => reject('p2 fail'), 500))
-const p3 = new Promise((resolve) => setTimeout(() => resolve('p3 finish'), 3000))
-
-Promise.myAny([p1, p2, p3])
-    .then(res => console.log('成功结果:', res)) // 预期输出: 成功结果: p1 finish
-    .catch(err => console.error('错误结果:', err));
-
-
-//const pErr1 = Promise.reject('错误A');
-//const pErr2 = Promise.reject('错误B');
+//const p1 = new Promise((resolve) => setTimeout(() => resolve('p1 finish'), 1000))
+//const p2 = new Promise((_, reject) => setTimeout(() => reject('p2 fail'), 500))
+//const p3 = new Promise((resolve) => setTimeout(() => resolve('p3 finish'), 3000))
 //
-//Promise.myAny([pErr1, pErr2])
-//    .then(res => console.log(res))
-//    .catch(err => {
-//        console.error('全失败触发:', err.message); // 预期输出: All promises were rejected
-//        console.log('详细错误列表:', err.errors);   // 预期输出: ['错误A', '错误B']
-//    });
+//Promise.myAny([p1, p2, p3])
+//    .then(res => console.log('成功结果:', res)) // 预期输出: 成功结果: p1 finish
+//    .catch(err => console.error('错误结果:', err));
+
+
+const pErr1 = Promise.reject('错误A');
+const pErr2 = Promise.reject('错误B');
+
+Promise.myAny([pErr1, pErr2])
+    .then(res => console.log(res))
+    .catch(err => {
+        console.error('全失败触发:', err.message); // 预期输出: All promises were rejected
+        console.log('详细错误列表:', err.errors);   // 预期输出: ['错误A', '错误B']
+    });
