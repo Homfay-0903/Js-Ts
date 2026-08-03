472bec81fd0da406c04d8365fae9249de2d6f774
Homfay <2790165595@qq.com>
2026-08-03 13:55:51 +0800

    update: limitConcurrency

--- promise/limitConcurrency.js
+++ promise/limitConcurrency.js
@@ -1,3 +1,4 @@
+/*
 function limitConcurrency(urls, maxLimit, fetchFn) {
     return new Promise((resolve) => {
         const result = []
@@ -44,3 +45,61 @@ const mockFetch = (url) => {
 }
 limitConcurrency(['u1', 'u2', 'u3', 'u4', 'u5', 'u6', 'u7', 'u8'], 3, mockFetch).then(console.log)
 '🚀✅'
+*/
+
+/**实现一个限制请求数量的函数 */
+/**
+ * 
+ * @param {String[]} reqList 
+ * @param {number} limit 
+ * @param {Function} reqFn 
+ * @returns 
+ */
+function limitConcurrency(reqList, limit, reqFn) {
+    return new Promise((resolve, reject) => {
+        const res = new Array(reqList.length)
+
+        let curIdx = 0, activeIdx = 0
+
+        function process() {
+            if (curIdx >= reqList.length) {
+                if (activeIdx === 0) {
+                    resolve(res)
+                }
+
+                return
+            }
+
+            let index = curIdx
+            let url = reqList[curIdx]
+            curIdx++, activeIdx++
+
+            reqFn(url)
+                .then(
+                    (val) => {
+                        res[index] = val
+                        activeIdx--
+                        process()
+                    },
+                    (err) => console.log(err)
+                )
+        }
+
+        const initialCount = Math.min(reqList.length, limit)
+
+        for (let i = 0; i < initialCount; i++) {
+            process()
+        }
+    })
+}
+
+function asyncTask(item) {
+    const delay = Math.random() * 10 * 1000
+    console.log(`need ${delay} ms`)
+    return new Promise(resolve => setTimeout(() => {
+        console.log(`${item} finish`)
+        resolve(item)
+    }, delay))
+}
+
+limitConcurrency([1, 2, 3], 2, asyncTask).then(console.log)
