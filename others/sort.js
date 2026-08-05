5d2c14130f1a0b395c92f111d38dfb47c2ae7ac9
Homfay <2790165595@qq.com>
2026-08-05 17:34:10 +0800

    update: sort-bubble

--- others/sort.js
+++ others/sort.js
@@ -1,18 +1,14 @@
 function bubbleSort(arr) {
-    const len = arr.length
-    for (let i = 0; i < len - 1; i++) {
-        let flag = false
-        for (let j = 0; j < len - i - 1; j++) {
+    const n = arr.length
+
+    for (let i = 0; i < n - 1; i++) {
+        for (let j = 0; j < n - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
                 [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
-                flag = true
             }
         }
-
-        if (!flag) {
-            break
-        }
     }
+
     return arr
 }
 
