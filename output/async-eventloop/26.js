5789f69ae1d61831bde859f45cf2053075e26dae
Homfay-0903 <2790165595@qq.com>
2026-09-26 20:11:09 +0800

    update: output-async-eventloop-26

---
+++ output/async-eventloop/26.js
@@ -0,0 +1,24 @@
+const p1 = new Promise((resolve) => {
+    setTimeout(() => {
+        resolve('resolve3');
+        console.log('timer1')
+    }, 0)
+    resolve('resovle1');
+    resolve('resolve2');
+}).then(res => {
+    console.log(res)
+    setTimeout(() => {
+        console.log(p1)
+    }, 1000)
+    //return '123'
+}).finally(res => {
+    console.log('finally', res)
+})
+
+//!!
+/**
+ * resovle1
+ * finally undefined
+ * timer1
+ * Promise { <fulfilled>: undefined }
+ */
